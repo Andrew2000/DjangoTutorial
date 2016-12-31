9ab3343f5070b281987c7703e00c58518ddad2ba
Andrew Lee <andrew.james.lee88@gmail.com>
2016-12-31 14:33:11 -0500

    voting page added

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'polls'
-urlpatterns = [
+urlpatterns = [ 
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
@@ -11,6 +11,6 @@ urlpatterns = [
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-        
-        ]
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+
+]
